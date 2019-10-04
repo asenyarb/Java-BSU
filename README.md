2623dfc41d777aa95ccee08bfdfe2cf41b9557ad
asenyarb <dimoniss00@gmail.com>
2019-10-04 14:07:13 +0300

    - Add task implementation - Update README.md

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
-# Lab_2.Formatter_class
+# Lab_2.Strings_processing
 This is a branch of the [Java-BSU](https://github.com/asenyarb/Java-BSU/) repository.
 
-##Lab 2 - **Formatter_class**.
+##Lab 2 - **String, StringBuffer, StringTokenizer classes**.
 Variant 4.
 # Problem
-Use Formatter class for Lab 1.
+The program asks for two lines and an integer number P to search for. 
+
+The first line contains tokens consisting of any characters that can be entered from the keyboard, e.g. a number (see individual variants, 2nd or 8th or 10th or 16th number).
+
+The second line contains separators (1 separator is 1 character, but there can be >1 separator and different separators between tokens).
 
 **Tasks**:
--   Print integer values in octal and hexadecimal form
--	Output floating point values
--   Use minimum field width specifier
--   Use accuracy specifier
--   Use flags of formatting (5 different flags)
--   Using the ordinal number of the argument
+- Divide the first line into tokens (using the separators from the second line)
+- Find all the decimals in it. Write these decimals into a new separate array. 
+- Among the tokens that are not decimals, find tokens consisting only of the same symbols. 
+- Find the number P (if any, it should coincide with the token), display the position in the original line. 
+- Duplicate one of the numbers in the string, add it after the number. 
+- Remove a substring from the string of punctuation characters, if any.  
+- Print all the results.
 
+*Use methods*:
+
+- For String: format, getChars, Split, Remove, Substring, Replace, IndexOf, LastIndexOf
+
+- For StringBuffer: delete, insert, reverse
--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,118 @@
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.text.Normalizer;
-import java.util.Formatter;
+import java.util.Arrays;
+import java.util.Optional;
 import java.util.Scanner;
-import java.util.Random;
-import static java.lang.Math.sqrt;
-
-import TaylorSeries.TaylorSeries;
-
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.printf("Array of numbers:\n%11s %15s | %15s | %15s", "" , "decimal", "oct", "hex");
-        int[] array_of_random_decimals = new int[5];
-        Random random = new Random();
-        Formatter formatter = new Formatter();
-        for (int i=0; i<5; i++){
-            array_of_random_decimals[i] = random.nextInt();
-            formatter.format("\nElement %2d: %2$+(015d | %2$#-15o | %2$#15x", i+1, array_of_random_decimals[i]);
-        }
-        System.out.println(formatter);
-        System.out.print("\nEnter x, belonging to ]-1,1[ : ");
         Scanner scan = new Scanner(System.in);
-        BigDecimal x = scan.nextBigDecimal();
-        System.out.print("Enter precision: ");
-        BigInteger precision = scan.nextBigInteger();
+        System.out.println("Enter a line:");
+        String line = scan.nextLine();
+        System.out.println("Enter delimiters:");
+        String delimiters = scan.nextLine();
+        System.out.println("Enter a number to find in first line:");
+        Integer P = scan.nextInt();
+        StringTokenizer st = new StringTokenizer(line, delimiters);
+
+        // Find all the decimals and find P
+        Integer [] decimalsTempArray = new Integer[st.countTokens()];
+        String [] nonDecimalsArray = new String[st.countTokens()];
+        int decimalIndex = 0;
+        int nonDecimalIndex = 0;
+        Integer mainIndex = -1;
+        Integer pIndex = null;
+        Integer decimal = null;
+        while (st.hasMoreTokens()){
+            mainIndex++;
+            var token = st.nextToken();
+            try {
+                decimal = Integer.parseInt(token, 10);
+            }
+            catch(NumberFormatException ex){
+                // For non-decimal values
+                boolean match = true;
+                char firstchar = token.charAt(0);
+                for (var ch: token.toCharArray())
+                    if (ch != firstchar) {
+                        match = false;
+                        break;
+                    }
+                if (match) {
+                    nonDecimalsArray[nonDecimalIndex] = token;
+                    nonDecimalIndex++;
+                }
+                continue;
+            }
+            // For P index
+            boolean eq;
+            try {
+                eq = decimal.equals(P);
+            }
+            catch(NullPointerException e){
+                eq = false;
+            }
+            if (eq) {
+                pIndex = mainIndex + 1;
+            }
+            // For decimal array
+            decimalsTempArray[decimalIndex] = decimal;
+            decimalIndex++;
+        }
+
+        Integer[] decimalArray = Arrays.copyOf(decimalsTempArray, decimalIndex);
+        String[] nonDecimals = Arrays.copyOf(nonDecimalsArray, nonDecimalIndex);
+
+        // Display decimals array
+        System.out.println("\nDecimals in the first line:");
+        for (Integer dec: decimalArray){
+            System.out.print(dec + " ");
+        }
+        System.out.println("\n");
+
+        // Display the P value
+        System.out.print("P-token index = ");
+        System.out.println((pIndex==null)?"P isn't found":pIndex);
+
+        // Display non-decimals with same characters array
+        System.out.println("\nNon-decimals in first line with same characters:");
+        for (String str: nonDecimals){
+            System.out.print(str + " ");
+        }
         System.out.println();
-        BigDecimal result = new TaylorSeries(x).calculate(precision);
-        Formatter formatter2 = new Formatter();
-        formatter2.format("%7.5f", result.doubleValue());
-        System.out.println("\n\nPrecised value is: " + result);
-        System.out.println("Same as float using formatter with .precision = 6: " + formatter2);
-        System.out.print("Calculated value is: " + sqrt(1+x.doubleValue()));
+
+        StringBuffer strb = new StringBuffer(line);
+        // Duplicate P value
+        if (decimalArray.length == 0){
+            System.out.println("There are no decimals in line. Nothing to duplicate");
+        }
+        else {
+            Integer decimalToDuplicate = decimalArray[0];
+            int duplicateDecimalIndexInLine = line.indexOf(decimalToDuplicate.toString()) + decimalToDuplicate.toString().length();
+            System.out.println("\nDuplicating number " + decimalToDuplicate + " at the position " + duplicateDecimalIndexInLine);
+            strb.insert(duplicateDecimalIndexInLine, delimiters.charAt(0) + decimalToDuplicate.toString());
+            line = strb.toString();
+            System.out.println("\nNew line after decimal duplicating: " + line);
+        }
+
+        // Delete all substrings consisting of punctuation characters
+        System.out.println("\nDeleting punctuation characters...");
+        String punctuationCharacters = ",;.";
+        for (Character ch: punctuationCharacters.toCharArray()){
+            if (delimiters.indexOf(ch) != -1){
+                System.out.println("Punctuation character " + ch + " belongs to delimiters string.");
+                System.out.println("Substrings with this character won't be deleted");
+            }
+            else
+            {
+                int charIndex = strb.toString().indexOf(ch);
+                while (charIndex != -1){
+                    strb.deleteCharAt(charIndex);
+                    charIndex = strb.toString().indexOf(ch);
+                }
+            }
+        }
+        line = strb.toString();
+        System.out.println("\nModificated line: " + line);
     }
 }
